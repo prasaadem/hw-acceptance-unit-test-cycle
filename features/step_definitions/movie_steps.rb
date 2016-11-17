c2f2cdb21bccc85e7e78e93639fcedc65ceb146e
Aditya Emani <prasaadem@gmail.com>
2016-11-17 13:06:22 +0000

    Scenario 1 Passed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -3,3 +3,8 @@ Given /the following movies exist:$/ do |movies_table|
         Movie.create!(title: hash['title'], rating: hash['rating'], release_date: hash['release_date'], director: hash['director'])
     end
 end
+
+Then /^the director of "(.*)" should be "([^"]*)"$/ do |movie_name, value|
+  expect(page).to have_content(movie_name)
+  expect(page.body).to match(/Director:\s#{value}/)
+end
